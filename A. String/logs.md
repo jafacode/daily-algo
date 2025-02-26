b4189828f3361d1cb1b49e7591f3ffb0e93f4802
Junghoon Kim <jafarock@naver.com>
2025-02-27 00:40:15 +0900

    2025-02-26

--- A. String/logs.md
+++ A. String/logs.md
@@ -2,3 +2,7 @@
 Started solving string, got the logic down just have to implement it. Having trouble implementing the finding the longest 1010...101 sequence.
 2025.02.24
 Continued programming, took a lot of time finding and relearning the fundamentals of c++.
+2025.02.25
+Finished coding, need to debug
+2025.02.26
+Kept debugging
--- A. String/string.cpp
+++ A. String/string.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 using namespace std;
 
-// Optional: Fast I/O for handling large inputs
-#define fast_io ios::sync_with_stdio(false); cin.tie(0);
-
 void solve() {
     // Read input
     string s;
-    int count;
+    int count = 0;
     cin >> s;
 
     if(s.find_first_not_of('0') == string::npos){
         cout << 0 <<endl;
+        return;
     }
 
     while(s.find_first_not_of('0') != string::npos){
         char key = '1';
         int invert[s.size()];
+        int invertIndex = 0;
         //create empty array of integers
 
         //for loop through s, if an index contains 1 set key to 0 save index to and array. if the index is '0' and key is '0' check the next character if that is also '0' do nothing, if it is '1' save the current index to the array
@@ -24,27 +23,45 @@ void solve() {
         for(int i=0; i<s.size(); i++){
             if(s[i] == key){
                 if(key == '1'){
-                    invert[count] = s[i]
-                } else if (key == '0' && i != s.size()-1){
+                    if(invertIndex == 0 || invert[invertIndex-1] == '0'){
+                        invert[invertIndex] = s[i];
+                        invertIndex++;
+                    }
+                } else if (key == '0' && i != s.size()-1 && invert[invertIndex-1] == '1'){
                     if(s[i+1] == '1'){
+                        invert[invertIndex]=s[i];
+                        invertIndex++;
                         //save current i in array
                     }
                 }
             }
         }
+        if (invert[invertIndex-1] == '0'){
+            invertIndex-=2;
+        } else {
+            invertIndex-=1;
+        }
         
         //go through array and switch each according s[i] to the opposite character.
         // count ++
+        for(int i=0; i<invertIndex; i++){
+            if(s[i]=='0'){
+                s[i] = '1';
+            } else {
+                s[i] = '0';
+            }
+        }
+        count++;
     }
 
     // Print output
-    cout << result << endl;
+    cout << count << endl;
+    return;
 }
 
 
 
 int main() {
-    fast_io;  // Enable fast I/O
     int t;  // Number of test cases
     cin >> t;
     while(t--){
